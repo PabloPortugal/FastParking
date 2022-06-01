f677692de70baede87397dd3eaa324a46d683ca9
PABLO PORTUGAL DA SILVA <21276389@CFP127ALUNO.LOCAL>
2022-06-01 16:57:36 -0300

    Adicionando informações

---
+++ app/src/main/java/com/example/fastparking/BuscarPlaca.kt
@@ -0,0 +1,11 @@
+package com.example.fastparking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BuscarPlaca : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`home.xml`)
+    }
+}
